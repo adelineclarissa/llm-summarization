49816727595f71dbf2fd981ef7bcb38ab91e8f3e
adelineclarissa <adeline.clarissa@gmail.com>
2025-01-28 13:32:56 -0700

    anonymization abstraction

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ NAME_PLACEHOLDER = "Tian"
 def setup_logging():
     # instantiate logger
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)  # Change this
 
     # formatter
     formatter = logging.Formatter(
@@ -44,24 +44,36 @@ def anonymize(
     phone_placeholder=PHONE_PLACEHOLDER,
 ):
 
-    # Split the name into components (first, middle, last)
+    original_name = original_name.replace("(", "").replace(")", "")
     name_parts = original_name.split()
-    name_pattern = r"\b" + r"\s+".join([rf"({part})?" for part in name_parts]) + r"\b"
+    name_pattern = (
+        r"\b("
+        + r"|".join(
+            [re.escape(part) for part in name_parts]
+            + [r"\s+".join(map(re.escape, name_parts))]
+        )
+        + r")\b"
+    )
+    name_with_parentheses_pattern = (
+        r"(" + r"\s+".join(map(re.escape, name_parts)) + r")\s*\(([^)]+)\)"
+    )
     name_regex = re.compile(name_pattern, re.IGNORECASE)
+    name_with_parentheses_regex = re.compile(
+        name_with_parentheses_pattern, re.IGNORECASE
+    )
     conversation = name_regex.sub(name_placeholder, conversation)
+    conversation = name_with_parentheses_regex.sub(
+        lambda match: f"{name_placeholder} ({name_placeholder})", conversation
+    )
 
     # Anonymize phone numbers
     phone_pattern = r"(?<!\d)(?:\+62|08)\d{9,}\b"
+
     phone_regex = re.compile(phone_pattern)
     original_phone_numbers = phone_regex.findall(conversation)
     conversation = phone_regex.sub(phone_placeholder, conversation)
 
     return conversation, original_name, original_phone_numbers
-
-
-# Prompt
-client_openai = openai.OpenAI()
-openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 def prompt_openai(text, m13id):
@@ -222,14 +234,66 @@ def prompt_openai(text, m13id):
         return None
 
 
-if __name__ == "__main__":
-    setup_logging()
-    logger = logging.getLogger(__name__)
+def prompt_summary(text, m13id):
+    prompt = [
+        {
+            "role": "user",
+            "content": f"""
+    You are a staff member of a non-profit mission agency. Please disregard previous knowledge and focus only on the given conversation between another staff member and a potential contact for evangelization. Extract the following data points in JSON format in Indonesian, with the specified keys:
+    Your task is to extract the following information and present it as a paragraph in Indonesian:
+    1. The name of the agent or mediator that was last to speak with the contact.
+    2. The contact’s initial felt need, what he/she asked or talked about in the beginning of the conversation.
+    3. Summarize the Agent’s expalnation about Isa Al-Masih (refer to Isa Al-Masih as IAM).  This explanation is aimed to console or give advice to the contact after they talk about their felt need.
+    4. Extract the contact's opinion about Isa Al-Masih according to the Gospel (IAM versi Injil)
 
-    folder_path = input("Please enter folder path: ")
-    excel_file = input("Please enter excel file for the output: ")
-    excel_file = utility.validate_excel(filename=excel_file)
+    Here is the conversation:
 
+    {text}
+
+    """,
+        }
+    ]
+    completion = client_openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=prompt,
+    )
+    output = completion.choices[0].message.content
+    logger.debug(output)
+    return output
+
+
+# This function cleans the HTML formatting and anonymize the text
+def anonymize_and_clean(file_path: str, m13id: str):
+    load_dotenv()
+    db_config = {
+        "host": os.getenv("DB_HOST"),
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "database": os.getenv("DB_NAME"),
+        "port": os.getenv("DB_PORT"),
+    }
+
+    # get the contact's name from database
+    db_manager = DatabaseManager(db_config=db_config)
+    db_manager.connect()
+    name_df = db_manager.fetch_name_by_m13(m13id=m13id)
+    name = name_df.to_string(index=False, header=False)
+
+    with open(file_path, "r") as file:
+        conversation = file.read()
+        conversation = utility.clean_html_styling(conversation)
+        clean_conversation, original_name, original_phone = anonymize(
+            conversation, name
+        )
+    db_manager.disconnect()
+    # Dump the clean conversation
+    with open(f"test-dump-2/{m13id}_clean.txt", "w") as f:
+        f.write(clean_conversation)
+
+    return clean_conversation, original_name, original_phone
+
+
+def main(folder_path: str, excel_file: str):
     total_files = len([f for f in os.listdir(folder_path) if f.endswith(".txt")])
     processed_files = 0
     skipped_ids = []
@@ -240,56 +304,34 @@ if __name__ == "__main__":
             try:
                 file_path = os.path.join(folder_path, file_name)
                 m13id = utility.get_file_id(file_path)
+                cleaned_text, original_name, original_phone = anonymize_and_clean(
+                    file_path, m13id
+                )
 
-                load_dotenv()
-                db_config = {
-                    "host": os.getenv("DB_HOST"),
-                    "user": os.getenv("DB_USER"),
-                    "password": os.getenv("DB_PASSWORD"),
-                    "database": os.getenv("DB_NAME"),
-                    "port": os.getenv("DB_PORT"),
-                }
+                output = prompt_openai(cleaned_text, m13id)
+                output = utility.clean_json(input_string=output)
+                contact = utility.parse_json_to_contact(json_data=output)
 
-                # get the contact's name from database
-                db_manager = DatabaseManager(db_config=db_config)
-                db_manager.connect()
-                name_df = db_manager.fetch_name_by_m13(m13id=m13id)
-                name = name_df.to_string(index=False, header=False)
-
-                # output for json dump
-                output = ""
-
-                with open(file_path, "r") as file:
-                    original_conversation = file.read()
-                    anonymized_text, original_name, original_phone = anonymize(
-                        original_conversation, name
-                    )
-
-                    cleaned_text = utility.clean_html_styling(anonymized_text)
-                    output = prompt_openai(cleaned_text, m13id)
-                    output = utility.clean_json(input_string=output)
-                    contact = utility.parse_json_to_contact(json_data=output)
-
-                    # prevent exceptions in the next block if contact is None
-                    if contact is not None:
-                        contact.id = m13id  # IMPORTANT
-                        contact.init_level()  # IMPORTANT: Initialize level
-                        if contact.name == NAME_PLACEHOLDER:
-                            logger.debug(f"Changed {contact.name} into {original_name}")
-                            contact.name = original_name
-                        if contact.phone_number == PHONE_PLACEHOLDER:
-                            logger.debug(
-                                f"Changed {contact.phone_number} into {original_phone}"
-                            )
-                            contact.phone_number = original_phone
-                        # parse contact and output it in excel
-                        success = utility.contact_to_excel(contact, excel_file)
-
-                    else:
-                        logger.error(
-                            f"Contact is None. Appending ID '{m13id}' to the skipped_id list."
+                # prevent exceptions in the next block if contact is None
+                if contact is not None:
+                    contact.id = m13id  # IMPORTANT
+                    contact.init_level()  # IMPORTANT: Initialize level
+                    if contact.name == NAME_PLACEHOLDER:
+                        logger.debug(f"Changed {contact.name} into {original_name}")
+                        contact.name = original_name
+                    if contact.phone_number == PHONE_PLACEHOLDER:
+                        logger.debug(
+                            f"Changed {contact.phone_number} into {original_phone}"
                         )
-                        skipped_ids.append(m13id)
+                        contact.phone_number = original_phone
+                    # parse contact and output it in excel
+                    success = utility.contact_to_excel(contact, excel_file)
+
+                else:
+                    logger.error(
+                        f"Contact is None. Appending ID '{m13id}' to the skipped_id list."
+                    )
+                    skipped_ids.append(m13id)
 
                 # LOG - output json dump into a txt file
                 dumpfile = f"test-output-dump/{m13id}-dump.txt"
@@ -299,8 +341,6 @@ if __name__ == "__main__":
                 # increment the counter and log the progress
                 processed_files += 1
                 logger.info(f"Processed {processed_files}/{total_files} files.")
-
-                db_manager.disconnect()
 
             except Exception as e:
                 # TODO: give more explanation about the exception
@@ -315,3 +355,16 @@ if __name__ == "__main__":
         logger.info(
             f"Successfully processed all files in the folder."
         )  # see if this works lol
+
+
+if __name__ == "__main__":
+    client_openai = openai.OpenAI()
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+    setup_logging()
+    logger = logging.getLogger(__name__)
+
+    folder_path = input("Please enter folder path: ")
+    excel_file = input("Please enter excel file for the output: ")
+    excel_file = utility.validate_excel(filename=excel_file)
+
+    main(folder_path, excel_file)
